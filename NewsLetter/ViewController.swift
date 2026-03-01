fda2bbddad151edbe394d33666e76025b790cde6
VintusS <dragonmd2003@gmail.com>
2026-03-01 21:27:42 +0200

    Initial Commit

---
+++ NewsLetter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsLetter
+//
+//  Created by dragomir.mindrescu on 01.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
